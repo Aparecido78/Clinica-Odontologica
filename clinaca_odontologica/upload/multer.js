3d6a0a3b803c709feae811a3c64d16faca725a4b
Geraldo <geraldosantana447@gmail.com>
2025-12-12 18:23:01 -0300

    Adiciona pasta upload com multer.js

---
+++ clinaca_odontologica/upload/multer.js
@@ -0,0 +1,17 @@
+const multer = require("multer")
+const path = require("path")
+const diskstorage = multer.diskStorage({
+    destination: (req,file,cb)=>{
+        cb(null,"./upload")
+    }
+    ,
+    filename: (req,file,cb)=>{
+       cb(null, Date.now()+path.extname(file.originalname))
+    }
+
+})
+
+
+const upload = multer({storage:diskstorage})
+
+module.exports = upload
