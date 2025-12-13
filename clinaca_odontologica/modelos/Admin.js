da94810049efc3b79273533b06d325e51f8cbec4
Geraldo <geraldosantana447@gmail.com>
2025-12-12 22:32:14 -0300

    atualização

--- clinaca_odontologica/modelos/Admin.js
+++ clinaca_odontologica/modelos/Admin.js
@@ -1,43 +1,47 @@
-const Sequelize = require("sequelize");
+const Sequelize = require("sequelize")
 const database = require("../configuracao/config");
+const Admin  = database.define("Admin",{
+    
+    nome:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    senha:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    foto:{
+        type: Sequelize.STRING
+    },
+        cpf:{
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        telefone:{
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        endereco:{
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        data_nascimento:{
+            type: Sequelize.DATEONLY,
+            allowNull: false
+        },
+        sexo:{
+            type: Sequelize.STRING,
+            allowNull: false
+        }
+})
 
-const Admin = database.define("Admin", {
-  nome: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  senha: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  foto: {
-    type: Sequelize.STRING,
-    allowNull: true
-  },
-  cpf: {
-    type: Sequelize.STRING,
-    allowNull: true  
-  },
-  telefone: {
-    type: Sequelize.STRING,
-    allowNull: true
-  },
-  endereco: {
-    type: Sequelize.STRING,
-    allowNull: true
-  },
-  data_nascimento: {
-    type: Sequelize.DATEONLY,
-    allowNull: true
-  },
-  sexo: {
-    type: Sequelize.STRING,
-    allowNull: true
-  }
-});
 
-module.exports = Admin;
+
+
+module.exports = Admin
+
+
