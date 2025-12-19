272b164285ff3fda2cec621556713294c70d95b1
Geraldo <geraldosantana447@gmail.com>
2025-12-19 14:20:07 -0300

    atualizar

--- clinaca_odontologica/configuracao/config.js
+++ clinaca_odontologica/configuracao/config.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,  
-  process.env.DB_USER,  
-  process.env.DB_PASS,   
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres', 
-    logging: false,      
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, 
+    },
+  },
+});
 
 sequelize.authenticate()
   .then(() => {
